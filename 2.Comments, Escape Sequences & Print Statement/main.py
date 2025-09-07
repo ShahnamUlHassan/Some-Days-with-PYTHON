e91ca285346abb5fac4ebeadac3f097cb1a29774
ShahnamUlHassan <ibrahim2baig786@gmail.com>
2025-09-07 19:25:26 +0500

    Add files via upload

---
+++ 2.Comments, Escape Sequences & Print Statement/main.py
@@ -0,0 +1,20 @@
+# Hello please don't remove this line
+# (This is just a reminder/comment line, it will not affect the program execution)
+
+# Printing "Hello!" and "World" on two separate lines using the escape character \n
+print("Hello! \nWorld")
+
+# Printing a simple message with a string inside the print() function
+print("Hello! This is a print statement:")
+
+# Printing a string that includes double quotes inside it
+# We use the escape character \" to include quotes within a string
+print("Hello , Are you \"John?\"\" by anychance")
+
+# Printing multiple items separated by a custom separator (~) 
+# and ending the line with a custom string "007\n" instead of the default newline
+print("Hello", 6, 7, sep="~", end="007\n")
+
+# Printing a simple string "Shahnam!"
+# This will appear right after the previous line (because we used end="007\n")
+print("Shahnam!")
