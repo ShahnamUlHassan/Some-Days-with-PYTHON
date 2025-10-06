3dd1cf08d2b4ef24970a1349d0cb4e5246932d3d
ShahnamUlHassan <ibrahim2baig786@gmail.com>
2025-10-06 20:00:47 +0500

    Add files via upload

---
+++ 32.Short hand if else statements/main.py
@@ -0,0 +1,28 @@
+# Assign values to variables 'a' and 'b'
+a = 330
+b = 3303
+
+# Short-hand if-else (Ternary Operator)
+# This will print:
+# "A" if a > b
+# "=" if a == b
+# "B" if a < b
+print("A") if a > b else print("=") if a == b else print("B")
+
+# Another shorthand if statement
+# Will print 9 if a is less than b
+print(9) if a < b else ""
+
+# Will print 9 if a is greater than b
+print(9) if a > b else ""
+
+# Assign values to c and d
+c = 9
+d = 5
+
+# Using ternary operator to assign value based on a condition
+# If a > b then e = 8, otherwise e = 0
+e = 8 if a > b else 0
+
+# Print the value of e
+print(e)
